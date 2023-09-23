bada2533ac5205fcfda6d4cf89925a68cd17564b
Dexrool <34430259+Dexrool@users.noreply.github.com>
2023-09-23 12:14:11 +0500

    Update dre.py

--- dre.py
+++ dre.py
@@ -1,14 +1,30 @@
-import blocksmith 
-with open("n.txt", "a") as f:
-    
-    def convert_to_hex(line):
-        decimal_value = str(line.strip())
-        hex_value = blocksmith.BitcoinWallet.generate_compressed_address(decimal_value)
-        return line.strip() + ' ' + hex_value
-    with open('number.txt', 'r+') as file:
-        lines = file.readlines()
-        file.seek(0)
-        for line in lines:
-            converted_line = convert_to_hex(line)
-            file.write(converted_line + '\n')
-            
+import blocksmith
+F = 40000000000075719660
+line_number = 0
+while F != 73786976294838206463:
+        
+        line_number += 1
+          # Номер строки, которую вы хотите извлечь
+
+        file_path = 'number.txt'  # Путь к файлу, из которого вы хотите извлечь строку
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+            if line_number <= len(lines):
+                line = lines[line_number - 1].strip()
+                print(f"{line}")
+            else:
+                print("Invalid line number.")
+            hex_value = blocksmith.BitcoinWallet.generate_compressed_address(line)
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+# Проверяем, что указанный номер строки не превышает число строк в файле
+        if line_number <= len(lines):
+            lines[line_number - 1] = line + ' ' + hex_value + '\n'
+
+            with open(file_path, 'w') as file:
+                file.writelines(lines)
+        else:
+            print("Invalid line number.")
